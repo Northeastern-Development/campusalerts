21fecb3c7e0095800a4ce377bd68321284773c2d
Trent Gaffney <tr.gaffney@northeastern.edu>
2018-08-22 13:06:01 -0400

    Got the loop working for an individual campus

--- themes/neudev/loops/loop-campus.php
+++ themes/neudev/loops/loop-campus.php
@@ -60,8 +60,6 @@ if($filter == ''){
 
 }else {
 
-	// echo($filter);
-	// echo '<br>';
 
 	$args = array(
 		 "post_type" => "nualerts",
@@ -72,50 +70,30 @@ if($filter == ''){
 	);
 
 	$res = query_posts($args);
-	//print_r($res);
-
-	$affected_campus = get_fields($res[0]->ID)['affected_campus'];
-
 
 	$response = "";	// an empty return will collapse the alert area to nothing
 
-	//$campus = ;
-
-
-
 	if(count($res) > 0){
-		// echo($filter);
-		// echo '<br>';
+
 			$response .= "<div><h2>University Alert!</h2><p>The Northeastern University System has issued the following alert(s).  Please be sure to read any associated information and contact your campus emergency services with any questions.</p><ul>";
 
+			$guide = '<li><a href="%s" title="%s, read more">%s For: %s - %s - Read More</a></li>';
 
 		foreach($res as $r){
 			$fields = get_fields($r->ID);
-			print_r($fields);
+			//print_r($fields);
 
-			// $guide = '<li><a href="%s" title="%s, read more">%s For: %s - %s - Read More</a></li>';
+			$affected_campus = $fields['affected_campus'][0]->post_name;
+			$campuses = $affected_campus;
+			//print_r($campuses);
 
 			$campus = "";
-			//$affected_campus = "";
-			//echo($response);
 
-			//echo $fields['affected_campus'][0]->post_name;
-
-			if($fields['affected_campus'] == $affected_campus){
-				$affected_campus = $fields['affected_campus'][0]->post_name;
-				$campuses = $affected_campus;
-				print_r($campuses);
-
-
-
-				$guide = '<li><a href="%s" title="%s, read more">%s For: %s - %s - Read More</a></li>';
+			if($campuses == $filter){
 
 				foreach($fields['affected_campus'] as $c){
 					$campus .= ($campus != ""?", ":"").$c->post_name;
-					//print_r($campus);
 				}
-				//print_r($filter);
-				//print_r($affected_campus);
 
 				$response .= sprintf(
 					 $guide
@@ -127,26 +105,12 @@ if($filter == ''){
 
 				);
 
-				if($campus == $filter){
-					echo($response);
-				}
-
-
 			}
 
-		$response .= "</ul></div>";
 		}
-
+		echo($response);
+		$response .= "</ul></div>";
 	}
-
-	// $thisCampus = query_posts($args);
-	// $thisCampusFields = get_fields($thisCampus[0]->ID);
-
-	//print_r($thisCampus);
-
-
-
-
 
 }
 
